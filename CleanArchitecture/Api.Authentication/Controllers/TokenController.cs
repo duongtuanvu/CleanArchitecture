55b437524ca1e864a6b8744b4593520bd86cec76
vudt <45599380+duongtuanvu@users.noreply.github.com>
2021-04-19 11:17:26 +0700

    Add Api.Authentication

---
+++ CleanArchitecture/Api.Authentication/Controllers/TokenController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Authentication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TokenController : ControllerBase
+    {
+        private readonly IJwtToken _jwtToken;
+
+        public TokenController(IJwtToken jwtToken)
+        {
+            _jwtToken = jwtToken;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return _jwtToken.GenerateToken();
+        }
+    }
+}
---
+++ CleanArchitecture/Api.Authentication/JwtSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Authentication
+{
+    public class JwtSettings
+    {
+        public string SecretKey { get; set; }
+        public int Expires { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+    }
+}
